8d31daa14746f5174c7e26b38ca9ddfc7d21b109
gtygo <gtydoit@gmail.com>
2019-12-18 19:52:05 +0800

    add create key file

--- kv.go
+++ kv.go
@@ -1,6 +1,9 @@
 package okv
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"os"
 	"sync"
 )
@@ -28,3 +31,24 @@ func New(o Options) *KV {
 	return kv
 }
 
+func (kv *KV) Set(key string, val []byte) error {
+
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	return kv.setStreamWithLock(key, bytes.NewReader(val), false)
+}
+
+func (kv *KV) setStreamWithLock(key string, r io.Reader, sync bool) error {
+
+}
+
+func (kv *KV) createKeyFileWithLock(key string) (*os.File, error) {
+	mode := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+	f, err := os.OpenFile(kv.Options.Path, mode, kv.FilePerm)
+	if err != nil {
+		return nil, fmt.Errorf("chmod %s", err)
+	}
+	return f, nil
+
+}
